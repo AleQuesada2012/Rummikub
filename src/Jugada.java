f95b261b29ee689a5b6492faf3da02f439665426
Dizabu(Dizabu) <135075679+Dizabu@users.noreply.github.com>
2023-10-11 16:12:07 -0600

    Update Jugada.java

--- src/Jugada.java
+++ src/Jugada.java
@@ -8,25 +8,43 @@ public class Jugada {
     }
 
     public boolean escaleravaldida(Jugada jugada) {
-        for (int i = 0; i < jugada.jugada.size() - 1; i++) {
-            Ficha ficha1 = jugada.jugada.get(i);
-            Ficha ficha2 = jugada.jugada.get(i + 1);
+        if(jugada.jugada.size()>=3) {
+            for (int i = 0; i < jugada.jugada.size() - 1; i++) {
+                Ficha ficha1 = jugada.jugada.get(i);
+                Ficha ficha2 = jugada.jugada.get(i + 1);
 
-            if (ficha2.getNum() > ficha1.getNum() + 1 || !ficha2.getColor().equals(ficha1.getColor())) {
-                return false;
+                if (ficha2.getNum() > ficha1.getNum() + 1 || !ficha2.getColor().equals(ficha1.getColor())) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+    public boolean secuenciavalida(Jugada jugada) {
+        if (jugada.jugada.size() >= 3) {
+            for (int i = 0; i < jugada.jugada.size(); i++) {
+                Ficha ficha1 = jugada.jugada.get(i);
+                Ficha ficha2 = jugada.jugada.get(i + 1);
+
+                if (ficha2.getNum() != ficha1.getNum() && ficha2.getColor().equals(ficha1.getColor())) return false;
+
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public int valorjugada(Jugada jugada){
+        int cont = 0;
+        if(jugada.escaleravaldida(jugada)==true && jugada.secuenciavalida(jugada)==true){
+            for(int i=0;i<jugada.jugada.size();i++){
+                Ficha ficha = jugada.jugada.get(i);
+                
+                cont+=ficha.getNum();
+
             }
         }
-        return true;
-    }
-    public boolean secuenciavalida(Jugada jugada){
-        for(int i=0; i<jugada.jugada.size();i++){
-            Ficha ficha1 = jugada.jugada.get(i);
-            Ficha ficha2 = jugada.jugada.get(i + 1);
-
-            if(ficha2.getNum() != ficha1.getNum() && ficha2.getColor().equals(ficha1.getColor()))return false;
-
-        }
-        return true;
+        return cont;
     }
 }
-
