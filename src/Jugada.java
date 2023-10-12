7f8663d75ca67c4fd4a0fc9e4879abcab91c07c3
Dizabu(Dizabu) <135075679+Dizabu@users.noreply.github.com>
2023-10-11 19:34:05 -0600

    Update Jugada.java
    Update

--- src/Jugada.java
+++ src/Jugada.java
@@ -3,15 +3,20 @@ import java.util.Vector;
 public class Jugada {
     private Vector<Ficha> jugada;
 
-    public void Jugada() {
+
+    public Jugada() {
         jugada = new Vector<>();
     }
 
-    public boolean escaleravaldida(Jugada jugada) {
-        if(jugada.jugada.size()>=3) {
-            for (int i = 0; i < jugada.jugada.size() - 1; i++) {
-                Ficha ficha1 = jugada.jugada.get(i);
-                Ficha ficha2 = jugada.jugada.get(i + 1);
+    public  Jugada(Vector<Ficha>lista){
+        jugada = lista;
+    }
+
+    public boolean escaleravaldida() {
+        if(this.jugada.size()>=3) {
+            for (int i = 0; i < this.jugada.size() - 1; i++) {
+                Ficha ficha1 = this.jugada.get(i);
+                Ficha ficha2 = this.jugada.get(i + 1);
 
                 if (ficha2.getNum() > ficha1.getNum() + 1 || !ficha2.getColor().equals(ficha1.getColor())) {
                     return false;
@@ -21,11 +26,11 @@ public class Jugada {
         }
         return false;
     }
-    public boolean secuenciavalida(Jugada jugada) {
-        if (jugada.jugada.size() >= 3) {
-            for (int i = 0; i < jugada.jugada.size(); i++) {
-                Ficha ficha1 = jugada.jugada.get(i);
-                Ficha ficha2 = jugada.jugada.get(i + 1);
+    public boolean secuenciavalida() {
+        if (this.jugada.size() >= 3) {
+            for (int i = 0; i < this.jugada.size(); i++) {
+                Ficha ficha1 = this.jugada.get(i);
+                Ficha ficha2 = this.jugada.get(i + 1);
 
                 if (ficha2.getNum() != ficha1.getNum() && ficha2.getColor().equals(ficha1.getColor())) return false;
 
@@ -35,16 +40,22 @@ public class Jugada {
         return false;
     }
 
-    public int valorjugada(Jugada jugada){
+    public int valorjugada(){
         int cont = 0;
-        if(jugada.escaleravaldida(jugada)==true && jugada.secuenciavalida(jugada)==true){
-            for(int i=0;i<jugada.jugada.size();i++){
-                Ficha ficha = jugada.jugada.get(i);
-                
+        if(this.escaleravaldida() && this.secuenciavalida()){
+            for(int i=0;i<this.jugada.size();i++){
+                Ficha ficha = this.jugada.get(i);
                 cont+=ficha.getNum();
 
             }
         }
         return cont;
     }
+    public Ficha getfichapos(int x){
+        if(this.jugada.size()<x){
+           return this.jugada.get(x);
+        }
+        return null;
+    }
 }
+
