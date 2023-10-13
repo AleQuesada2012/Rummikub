90baecd0376fbb8611fe8b04ecd993fb2928f699
Dizabu(Dizabu) <135075679+Dizabu@users.noreply.github.com>
2023-10-12 23:44:34 -0600

    Update Mesa.java
    update

--- src/Mesa.java
+++ src/Mesa.java
@@ -1,28 +1,74 @@
+import java.util.Vector;
+
 public class Mesa {
     private Ficha[][] matrizFichas;
+
+    private Pila fichas;
 
     private int espaciolleno;
 
     public Mesa() {
+        fichas = new Pila();
+        matrizFichas = new Ficha[15][15];
+    }
+
+    public Mesa (int x){
         matrizFichas = new Ficha[15][15];
     }
 
 
-    public void ingresarjugada(Jugada jugada) {
-        if (jugada.escaleravaldida() && jugada.secuenciavalida()) {
-            int fila = espaciolleno;
-            if (matrizFichas[fila][0] != null) {
-                espaciolleno++;
-            } else {
-                for (int i = 0; i < jugada.getJugada().size(); i++) {
-                    matrizFichas[fila][i] = jugada.getfichapos(i);
+
+
+
+    public void ingresarFicha(Ficha ficha, int x, int y) {
+        matrizFichas[x][y] = ficha;
+    }
+
+    public boolean matrizValida(){
+        if(estaVacia())return true;
+
+        boolean esvalida = false;
+        Vector<Ficha> fichas =  new Vector<>();
+        for(int i = 0; i < 15; i++){
+            for(int j = 0; j < 15; j++){
+                if(matrizFichas[i][j]!=null){
+                    fichas.add(matrizFichas[i][j]);
+                }
+                if(matrizFichas[i][j] == null){
+                    Jugada posibleJugada = new Jugada(fichas);
+                    if(posibleJugada.serieValida() || posibleJugada.escaleraValida()){
+                        esvalida = true;
+                        fichas.clear();
+                    }
+                    else{
+                        return false;
+                    }
+
+                }
+
+
+            }
+
+        }
+        return esvalida;
+    }
+
+    public boolean estaVacia(){
+        for(int i = 0; i < 15; i++){
+            for(int j = 0; j < 15; j++){
+
+                if(matrizFichas[i][j]!= null){
+                    return false;
                 }
             }
         }
-
+        return true;
     }
-    public void copiarmesa(Mesa Mesaoriginal) {
-        for (int fila = 0; fila< matrizFichas.length; fila++) {
+
+
+
+    public void copiarMesa(Mesa Mesaoriginal) {
+        for (int fila = 0; fila < matrizFichas.length; fila++) {
             for (int col = 0; col < matrizFichas[fila].length; col++) {
                 matrizFichas[fila][col] = Mesaoriginal.matrizFichas[fila][col];
             }
@@ -30,6 +76,31 @@ public class Mesa {
         espaciolleno = Mesaoriginal.espaciolleno;
     }
 
+    public Ficha agarrarpila(){
+       return  fichas.agarrarficha();
+    }
+
+    public Pila getFichas() {
+        return fichas;
+    }
 
 
+
+    public void imprimirMesa() {
+        for (int fila = 0; fila < matrizFichas.length; fila++) {
+            for (int columna = 0; columna < matrizFichas[fila].length; columna++) {
+                Ficha ficha = matrizFichas[fila][columna];
+                if (ficha != null) {
+                    System.out.print(ficha.getNum() + " " + ficha.getColor() + "\t");
+                } else {
+                    System.out.print("-\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public Ficha[][] getMatrizFichas() {
+        return matrizFichas;
+    }
 }
