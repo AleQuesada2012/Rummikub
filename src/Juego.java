0a67e5cb2bf743f0e7b8bd526cf368eb898b7b89
Dizabu(Dizabu) <135075679+Dizabu@users.noreply.github.com>
2023-10-12 23:10:34 -0600

    Update Juego.java
    update Juego class

--- src/Juego.java
+++ src/Juego.java
@@ -4,24 +4,32 @@ public class Juego {
 
     private Mesa Tablero;
 
-
     private Mesa temporalmesa;
-    private Vector<Jugador>jugadores;
 
-    private Pila fichas;
+    private Vector<Jugador> jugadores;
 
 
     public Juego() {
-        fichas = new Pila();
-        fichas.generarpila();
+        Tablero = new Mesa();
+        temporalmesa = new Mesa(1);
+        jugadores = new Vector<>();
     }
-    private int determinarorden(Vector<Jugador> jugadores) {
+
+    public void setJugadores(Vector<Jugador> jugadores) {
+        this.jugadores = jugadores;
+    }
+
+    public void agregarjugador(Jugador jugador){
+        jugadores.add(jugador);
+    }
+
+    public int determinarOrden() {
         int maxficha = -1;
         int indicedeprimerjugador = 0;
 
-        for (int i = 0; i < jugadores.size(); i++) {
-            Jugador jugador = jugadores.get(i);
-            jugador.agregarFicha(fichas.agarrarficha());
+        for (int i = 0; i < this.jugadores.size(); i++) {
+            Jugador jugador = this.jugadores.get(i);
+            jugador.agregarFicha(Tablero.agarrarpila());
             for (Ficha ficha : jugador.getFichasEnMano().getFichas()) {
                 if (ficha.getNum() > maxficha) {
                     maxficha= ficha.getNum();
@@ -33,7 +41,7 @@ public class Juego {
         return indicedeprimerjugador;
     }
 
-    public void sumarpuntos(){
+    public void sumarPuntos(){
         int cont = 0;
         Jugador ganador = null;
         for(Jugador jugador : jugadores){
@@ -44,20 +52,42 @@ public class Juego {
             }
             else {
                 jugador.setPuntos(-(jugador.getFichasEnMano().getsumadefichas()));
-                cont+=jugador.getFichasEnMano().getsumadefichas();
+                cont += jugador.getFichasEnMano().getsumadefichas();
 
             }
         }
         if(ganador!=null){
             ganador.setPuntos(cont);
         }
+    }
 
+    public void agarrarfichas(){
+        for(Jugador jugador :  jugadores) {
+            for (int i = 0; i < 13; i++) {
+                jugador.agregarFicha((Tablero.agarrarpila()));
+            }
+        }
 
     }
 
 
+    public void actualizarMesa(){
+        if(temporalmesa.matrizValida()){
+            Tablero.copiarMesa(temporalmesa);
+        }
+    }
 
+    public void terminarTurno(){
+        actualizarMesa();
 
+    }
 
+    public Mesa getTablero() {
+        return Tablero;
+    }
+
+    public Vector<Jugador> getJugadores() {
+        return this.jugadores;
+    }
 }
 
